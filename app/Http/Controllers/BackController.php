6101ca51c562ba4309fac9c6e37f7709580ea694
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-01 05:59:09 -0800

    update

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use App\Models\Post;
 
 class BackController extends Controller
 {
-    //
+    public function generate_post()
+    {
+        $array_random = ['5', '6', '7', '8', '9', '10'];
+        $faker = Faker::create('id_ID');
+        $post = new Post;
+        $savePost = $post->create([
+            'post_judul' => $faker->word(Arr::random($array_random), true),
+        ]);
+    }
 }
--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Faker\Factory as Faker;
 
 class FrontController extends Controller
 {
@@ -13,6 +14,10 @@ class FrontController extends Controller
 
     public function index()
     {
-        return view('dashboard.index');
+        $faker = Faker::create('id_ID');
+        $data = [
+            'nama' => $faker->name
+        ];
+        return view('dashboard.index', compact('data'));
     }
 }
