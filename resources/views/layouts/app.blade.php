7e0cb55cc40f2344a45563332c3a6cac0ca797a5
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-02 01:20:51 -0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -185,6 +185,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
   <script src="{{ asset('assets/stisla/assets/js/stisla.js') }}"></script>
+  {{-- <script src="{{ asset('assets/stisla/assets/js/page/forms-advanced-forms.js') }}"></script> --}}
 
   <!-- JS Libraies -->
   {{-- <script src="../node_modules/simpleweather/jquery.simpleWeather.min.js"></script>
--- resources/views/livewire/add-post.blade.php
+++ resources/views/livewire/add-post.blade.php
@@ -1,11 +1,14 @@
 <div>
     @section('header-title', 'Tambah Post')
+    @section('after-css')
+    <script src="https://cdn.tiny.cloud/1/0fzrtif8pxlg6kw3rfi13s2t5xzfaiqpavx3fiqci9ysvmva/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    @endsection
 
     <div class="container">
 
         <div class="row">
 
-            <div class="col-8 col-md-8 col-lg-8">
+            <div class="col-sm-12 col-md-8 col-lg-8">
                 
                 <div class="card">
                     <div class="card-body">
@@ -46,14 +49,21 @@
 
 
 
-            <div class="col-4 col-md-4 col-lg-4">
+            <div class="col-sm-12 col-md-4 col-lg-4">
                 
                 <div class="card">
                     <div class="card-body">
                         <div class="form-group">
-                            <label>Default Input Text</label>
-                            <input type="text" class="form-control">
-                        </div>
+                            <label>Date Range Picker</label>
+                            <div class="input-group">
+                              <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                  <i class="fas fa-calendar"></i>
+                                </div>
+                              </div>
+                              <input type="date" class="form-control daterange-cus">
+                            </div>
+                          </div>
                         <div class="form-group">
                             <label>Phone Number (US Format)</label>
                             <div class="input-group">
@@ -87,49 +97,34 @@
 
 
 
-            <div class="col-12 col-md-12 col-lg-12">
+            <div class="col-sm-12 col-md-12 col-lg-12">
                 
                 <div class="card">
                     <div class="card-body">
                         <div class="form-group">
                             <label>Default Input Text</label>
-                            <input type="text" class="form-control">
+                            <textarea name="post_body">
+                            </textarea>
                         </div>
-                        <div class="form-group">
-                            <label>Phone Number (US Format)</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fas fa-phone"></i>
-                                    </div>
-                                </div>
-                                <input type="text" class="form-control phone-number">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>Password Strength</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="fas fa-lock"></i>
-                                </div>
-                            </div>
-                            <input type="password" class="form-control pwstrength" data-indicator="pwindicator">
-                        </div>
-                        <div id="pwindicator" class="pwindicator">
-                            <div class="bar"></div>
-                            <div class="label"></div>
-                        </div>
-                    </div>
                     </div>
                 </div>
 
             </div>
-
-
 
         </div>
 
     </div>
+@section('after-js')
 
+<script>
+    $(document).ready(function(){
+        tinymce.init({
+          selector: 'textarea',
+          plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+          toolbar_mode: 'floating',
+       });
+});
+</script>
+    
+@endsection
 </div>
