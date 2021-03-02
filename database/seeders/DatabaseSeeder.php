523374470c8719aa97202a611629e475bfbe6d65
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-02 06:25:38 -0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Kategori;
+use App\Models\Detail;
+use App\Models\Login;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        $detail = new Detail;
+        $saveDetail = $detail->create([
+            'detail_nama' => 'Administrator',
+            'detail_telepon' => '085342072185',
+            'detail_jeniskelamin' => 'L',
+        ]);
+
+        $login = new Login;
+        $saveLogin = $login->create([
+            'username' => 'admin',
+            'password' => 'admin',
+            'email' => 'admin@sultra1news.com',
+            'email' => 'admin@sultra1news.com',
+        ]);
+
         $kategori_array = [
             'Nasional',
             'Internasional',
