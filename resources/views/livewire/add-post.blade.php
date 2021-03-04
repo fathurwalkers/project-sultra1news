84749d5f5ad2aa6cc5e79be8edabc36e92463c95
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-04 08:16:56 -0800

    update

--- resources/views/livewire/add-post.blade.php
+++ resources/views/livewire/add-post.blade.php
@@ -40,6 +40,16 @@
                                 {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                                     Open Form
                                 </button> --}}
+
+                                <div class="gallery gallery-fw" data-item-height="100">
+                                    <div class="gallery-item" data-image="{{ asset('assets/stisla/') }}assets/img/news/img09.jpg" data-title="Image 1" href="assets/img/news/img09.jpg" title="Image 1" style="height: 100px; background-image: url(&quot;assets/img/news/img09.jpg&quot;);"></div>
+                                    <div class="gallery-item" data-image="{{ asset('assets/stisla/') }}assets/img/news/img10.jpg" data-title="Image 2" href="assets/img/news/img10.jpg" title="Image 2" style="height: 100px; background-image: url(&quot;assets/img/news/img10.jpg&quot;);"></div>
+                                    <div class="gallery-item gallery-more" data-image="{{ asset('assets/stisla/') }}assets/img/news/img08.jpg" data-title="Image 3" href="assets/img/news/img08.jpg" title="Image 3" style="height: 100px; background-image: url(&quot;assets/img/news/img08.jpg&quot;);">
+                                      <div style="line-height: 100px;">+2</div>
+                                    </div>
+                                    <div class="gallery-item gallery-hide" data-image="{{ asset('assets/stisla/') }}assets/img/news/img01.jpg" data-title="Image 4" href="assets/img/news/img01.jpg" title="Image 4" style="height: 100px; background-image: url(&quot;assets/img/news/img01.jpg&quot;);"></div>
+                                  </div>
+
                             </div>
                         </div>
 
