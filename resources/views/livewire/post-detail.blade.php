aebced530f4b5b8aaae1a1089fcf606c6eda58c1
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-04 04:04:34 -0800

    update

--- resources/views/livewire/post-detail.blade.php
+++ resources/views/livewire/post-detail.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{ $post->post_judul }}
+    @section('title', 'Post Detail')
+    @section('header-title', 'Post Detail')
+
+    {{-- Content Section  --}}
+    <div class="row">
+        <div class="col-sm-12 col-md-8 col-lg-8">
+            
+            <div class="card">
+                <div class="card-body">
+
+                    
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
 </div>
